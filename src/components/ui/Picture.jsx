a8ef9e736b2c140194650a1b8120c9ae63dc0eff
Iair Kaplun <iairkap@gmail.com>
2024-01-18 21:52:12 -0300

    push

---
+++ src/components/ui/Picture.jsx
@@ -0,0 +1,12 @@
+/* import React from "react";
+import iair from "../../../src/iairkap-linkedin.jpeg";
+function Picture(props) {
+  return (
+    <div>
+      <img src="iair" alt="iair" width={80} height={80} />
+    </div>
+  );
+}
+
+export default Picture;
+ */
