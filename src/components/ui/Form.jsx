a55c9a16976e7d8e732da0f22696877e2ea2884e
Iair Kaplun <iairkap@gmail.com>
2024-01-19 00:04:27 -0300

    updates

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useFormik } from "formik";
+import React, { useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import * as Yup from "yup";
 import styles from "../styles/form.module.css";
 import TextareaAutosize from "react-textarea-autosize";
+import rocket from "../../../public/rocket.svg";
 
 const projectTypes = ["Web App", "UX/UI", "Marketing", "Mobile App", "Other"];
 const servicesNeeded = [
@@ -24,53 +25,116 @@ const validationSchema = Yup.object({
 function Form() {
   const [selectedProjectTypes, setSelectedProjectTypes] = useState([]);
   const [selectedServicesNeeded, setSelectedServicesNeeded] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [text, setText] = useState("");
-  const [rows, setRows] = useState(1);
+  const [state, handleSubmit] = useForm("mayrnzvj");
+  const [submitStatus, setSubmitStatus] = useState("success");
 
-  const formik = useFormik({
-    initialValues: {
-      projectTypes: [],
-      servicesNeeded: [],
-      email: "",
-      name: "",
-      idea: "",
-    },
-    validationSchema,
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+  const validate = (values) => {
+    let errors = {};
+    try {
+      validationSchema.validateSync(values, { abortEarly: false });
+    } catch (error) {
+      error.inner.forEach((validationError) => {
+        errors[validationError.path] = validationError.message;
+      });
+    }
+    return errors;
+  };
 
   const handleProjectTypeClick = (type) => {
-    formik.setFieldValue(
-      "projectTypes",
-      formik.values.projectTypes.includes(type)
-        ? formik.values.projectTypes.filter((t) => t !== type)
-        : [...formik.values.projectTypes, type]
-    );
+    if (selectedProjectTypes.includes(type)) {
+      setSelectedProjectTypes(
+        selectedProjectTypes.filter((item) => item !== type)
+      );
+    } else {
+      setSelectedProjectTypes([...selectedProjectTypes, type]);
+    }
   };
 
   const handleServiceNeededClick = (service) => {
-    formik.setFieldValue(
-      "servicesNeeded",
-      formik.values.servicesNeeded.includes(service)
-        ? formik.values.servicesNeeded.filter((s) => s !== service)
-        : [...formik.values.servicesNeeded, service]
-    );
+    if (selectedServicesNeeded.includes(service)) {
+      setSelectedServicesNeeded(
+        selectedServicesNeeded.filter((item) => item !== service)
+      );
+    } else {
+      setSelectedServicesNeeded([...selectedServicesNeeded, service]);
+    }
   };
-  useEffect(() => {
-    setRows(text.split("\n").length);
-  }, [text]);
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const errors = validate({
+      projectTypes: selectedProjectTypes,
+      servicesNeeded: selectedServicesNeeded,
+      email: email,
+      name: name,
+      idea: text,
+    });
+    if (Object.keys(errors).length === 0) {
+      fetch("https://formspree.io/f/mayrnzvj", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          projectTypes: selectedProjectTypes,
+          servicesNeeded: selectedServicesNeeded,
+          idea: text,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            setSubmitStatus("success");
+          } else {
+            throw new Error("Error al enviar el correo electrónico");
+          }
+        })
+        .catch((error) => {
+          setSubmitStatus("error");
+        });
+    }
+  };
+
+  if (submitStatus === "success") {
+    return (
+      <div className={styles.subtitles}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            gap: "1rem",
+          }}
+        >
+          <h1 className={styles.thankYouTitle}>Thank You for Trusting Us!</h1>
+        </div>
+
+        <p className={styles.thankYouMessage}>
+          We appreciate your project submission and can't wait to bring your
+          ideas to life. Our team will get in touch with you shortly.
+        </p>
+      </div>
+    );
+  } else if (submitStatus === "error") {
+    return (
+      <h1>
+        Error al enviar el correo electrónico, contáctate a iairkap@gmail.com
+      </h1>
+    );
+  }
   return (
     <div>
-      <form onSubmit={formik.handleSubmit}>
+      <form onSubmit={onSubmit}>
         <h4 className={styles.subtitles}>Your Project Types:</h4>
         <div className={styles.buttonContainer}>
           {projectTypes.map((type) => (
             <button
               className={`${styles.buttons} ${
-                formik.values.projectTypes.includes(type) ? styles.selected : ""
+                selectedProjectTypes.includes(type) ? styles.selected : ""
               }`}
               type="button"
               onClick={() => handleProjectTypeClick(type)}
@@ -84,9 +148,7 @@ function Form() {
           {servicesNeeded.map((service) => (
             <button
               className={`${styles.buttons} ${
-                formik.values.servicesNeeded.includes(service)
-                  ? styles.selected
-                  : ""
+                selectedServicesNeeded.includes(service) ? styles.selected : ""
               }`}
               type="button"
               onClick={() => handleServiceNeededClick(service)}
@@ -102,16 +164,16 @@ function Form() {
             placeholder="Your Name"
             type="text"
             name="name"
-            onChange={formik.handleChange}
-            value={formik.values.name}
+            onChange={(e) => setName(e.target.value)}
+            value={name}
           />
           <input
             className={styles.input}
             placeholder="Your Email"
             type="email"
             name="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
           <TextareaAutosize
             className={styles.text}
@@ -120,7 +182,11 @@ function Form() {
             onChange={(e) => setText(e.target.value)}
           />
         </div>
-        <button type="submit" className={styles.submit}>
+        <button
+          type="submit"
+          disabled={state.submitting}
+          className={styles.submit}
+        >
           Submit
         </button>
       </form>
@@ -129,3 +195,5 @@ function Form() {
 }
 
 export default Form;
+
+/*  className={styles.submit} */
