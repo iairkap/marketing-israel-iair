90d47604a2e6bc7f431d51d975620c16de064a71
Iair Kaplun <iairkap@gmail.com>
2024-01-20 18:05:25 -0300

    vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
---
+++ src/javascript/questions.js
@@ -0,0 +1,18 @@
+export const faqs = [
+  {
+    question: "How Much Do You Charge for a Project?",
+    answer:
+      "Unlocking the potential of your online presence is an art, and at our studio, we believe in crafting unique digital experiences tailored exclusively for you. The cost of our website design services is as distinctive as your project. We delve into your requirements, explore the intricacies of your vision, and then present you with a bespoke quote. No two websites are the same, and neither are our solutions – because your online identity deserves a design as exceptional as your business.",
+  },
+  {
+    question:
+      "What is the estimated duration for creating a website from scratch?",
+    answer:
+      "Think of your website's development as a captivating 4-6 week journey, intricately shaped by the nuances of your project. Much like creating a distinctive architectural masterpiece, building a website is a tailored art form designed to meet your specific needs. Whether it's a user-friendly digital space or a comprehensive online hub, the timeline adapts to your project's unique intricacies and requirements.",
+  },
+  {
+    question: "How are payments handled??",
+    answer:
+      "We divide the total into two parts. The initial 50% is invoiced prior to the project commencement, and the remaining 50% is invoiced upon completion. The final payment is processed only when you are completely satisfied with the delivered product and no further adjustments are needed.",
+  },
+];
