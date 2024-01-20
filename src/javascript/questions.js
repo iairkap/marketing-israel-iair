69aa5b006d97e8587ebe6793300788e4cffd92f8
Iair Kaplun <iairkap@gmail.com>
2024-01-20 19:17:11 -0300

    questionUpdates

--- src/javascript/questions.js
+++ src/javascript/questions.js
@@ -6,13 +6,23 @@ export const faqs = [
   },
   {
     question:
-      "What is the estimated duration for creating a website from scratch?",
+      "What is The Estimated Duration For Creating a Website From Scratch?",
     answer:
       "Think of your website's development as a captivating 4-6 week journey, intricately shaped by the nuances of your project. Much like creating a distinctive architectural masterpiece, building a website is a tailored art form designed to meet your specific needs. Whether it's a user-friendly digital space or a comprehensive online hub, the timeline adapts to your project's unique intricacies and requirements.",
   },
   {
-    question: "How are payments handled??",
+    question: "How do we stay connected throughout the project?",
+    answer:
+      "We maintain crystal-clear processes for both our team and clients. Plus, you'll have exclusive time-tracking access, keeping you in the loop on milestones, weekly sprints, and task durations.",
+  },
+  {
+    question: "How Are Payments Handled??",
     answer:
       "We divide the total into two parts. The initial 50% is invoiced prior to the project commencement, and the remaining 50% is invoiced upon completion. The final payment is processed only when you are completely satisfied with the delivered product and no further adjustments are needed.",
+  },
+  {
+    question: "Can I preview the design before development kicks in?",
+    answer:
+      "Yes, we offer a detailed design preview before we begin the development process. This allows you to get a feel for the design and make any necessary changes before we start coding.",
   },
 ];
