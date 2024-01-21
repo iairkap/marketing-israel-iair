00380ee645951987e26fcced24c7ea183f34ddcb
Iair Kaplun <iairkap@gmail.com>
2024-01-21 20:58:54 -0300

    i18-secondOption

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import icon from "astro-icon";
-import astroI18next from "astro-i18next";
-
+/* import astroI18next from "astro-i18next";
+ */
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), icon(), astroI18next()],
+  integrations: [react(), icon()],
   adapter: vercel(),
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "he"],
+  },
 });
